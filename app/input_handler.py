14aea0650f05baf3a156ce0383f006ca506c84cd
SensibleN00B <sensible.noob@gmail.com>
2025-07-10 14:31:35 +0300

    added comment in handle_input

--- app/input_handler.py
+++ app/input_handler.py
@@ -6,6 +6,10 @@ class InputHandler:
     def handle_input(prompt: str, data_type: type = str, make_capital: bool = False) -> Any:
         while True:
             try:
+                # I wanted to implement this construction to immediately return the first name\last name in uppercase,
+                # so that in the console when handling errors they would also be displayed in uppercase.
+                # But doesn't this violate the Single Responsibility Principle?
+
                 # if data_type == str and make_capital:
                 #     return data_type(input(prompt).capitalize())
                 return data_type(input(prompt))
